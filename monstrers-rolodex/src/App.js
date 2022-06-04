b652447e0516ad8393a7fdee218f20fb408c4675
Stel Zac <laiferisaac@gmail.com>
2022-06-04 18:17:59 +0200

    filter system

--- monstrers-rolodex/src/App.js
+++ monstrers-rolodex/src/App.js
@@ -10,27 +10,39 @@ class App extends Component {
     this.state = {
       monsters:[],
     };
-    console.log('constructor');
+    // console.log('constructor');
   }
 
   componentDidMount(){
-    console.log('componentDidMount')
+    // console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(() => {
       return {monsters: users}
     },
     () => {
-      console.log(this.state)
+      // console.log(this.state)
     }
     ));
   }
 
   render() {
-    console.log('render')
+    // console.log('render')
     return (
       <div className="App">
-        <input />
+        <input className='search-box' type='saerch' placeholder='sarch monsters' onChange={(event) => {
+          // console.log(event.target.value);
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          });
+
+          this.setState(() => {
+            return { monsters: filteredMonsters};
+          })
+        
+        }}
+        />
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}> <h1>{monster.name}</h1></div>
