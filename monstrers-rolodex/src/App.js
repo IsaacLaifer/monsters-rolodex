48d176ef22a058ccefd2ad56d85d8ebe12fc4119
Stel Zac <laiferisaac@gmail.com>
2022-06-04 19:23:53 +0200

    optimisation

--- monstrers-rolodex/src/App.js
+++ monstrers-rolodex/src/App.js
@@ -4,46 +4,42 @@ import './App.css';
 import { render } from '@testing-library/react';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      monsters:[],
+      monsters: [],
       searchField: '',
     };
-    // console.log('constructor');
+  }
+  
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
   }
 
-  componentDidMount(){
-    // console.log('componentDidMount');
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(() => {
-      return {monsters: users}
-    },
-    () => {
-      // console.log(this.state)
-    }
-    ));
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+      ));
   }
 
   render() {
-    // console.log('render')
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const{ monsters, searchField} = this.state;
+    const{ onSearchChange }= this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          // console.log(event.target.value);
-          const searchField = event.target.value.toLocaleLowerCase();
-          this.setState(() => {
-            return { searchField };
-          })
-        
-        }}
-        />
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
         {
           filteredMonsters.map((monster) => {
             return <div key={monster.id}> <h1>{monster.name}</h1></div>
