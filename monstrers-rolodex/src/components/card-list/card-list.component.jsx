26057340e124de48e03a868e332e35350311cdd0
Stel Zac <laiferisaac@gmail.com>
2022-06-06 15:17:34 +0200

    Last modif

--- monstrers-rolodex/src/components/card-list/card-list.component.jsx
+++ monstrers-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 
 import './card-list.style.css'
 
@@ -10,17 +11,12 @@ class CardList extends Component{
         const { monsters } = this.props; 
      
        return(
-           
            <div className="card-list">
             {monsters.map((monster) => {
-                const { name, email, id} = monster;
                 return(
-                <div className="card-container" key={id}>
-                    <img alt={`monster ${monster.name}`} src={`https://robohash.org/${id}?set=set3&size=180x180`}/>
-                    <h2>{monster.name}</h2>
-                    <p>{monster.email}</p>
-                </div>
-            )})}
+                    <Card monster={monster}/>
+            );
+            })}
            </div>
        )
     }
